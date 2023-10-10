c93920745478ef3767ba8605292085917def50b5
iceyisaak <me@iceyisaak.com>
2023-10-10 11:55:08 +0200

    Added styling to SearchPage

--- src/pages/search-page/search-page.tsx
+++ src/pages/search-page/search-page.tsx
@@ -1,11 +1,13 @@
 // import { useParams } from "react-router-dom";
-import { useSearchParams } from "react-router-dom";
+import { Link, useSearchParams } from "react-router-dom";
 // import { SearchBar } from "../../components/utilitiy-components/search-bar";
 import { MOVIE_POSTER_URL } from "../../api/api-constant";
 import { searchMovies } from "../../api/movie-api";
 import { SearchSection } from "../../components/sections/main-section/search-section";
 import { Breadcrumbs } from "../../components/utilitiy-components/breadcrumbs/breadcrumbs";
 import { MovieData } from "../../types";
+
+import style from './search-page.module.scss'
 
 export function SearchPage() {
 
@@ -26,24 +28,28 @@ export function SearchPage() {
             <Breadcrumbs
                 data={SearchMoviesData as MovieData}
             />
-            <section className="mt-32 flex flex-wrap bg-red-100">
+            <section className={`${style.search__page} mt-32 mb-40 flex flex-wrap bg-red-100}`}>
                 {
                     SearchMoviesData?.results.map((movie) => (
-                        <div key={movie.id} className="bg-green-200 w-80">
-                            <img
-                                src={`${movie.poster_path ?
-                                    `${MOVIE_POSTER_URL}${movie.poster_path}` :
-                                    'https://placehold.co/200x300?text=No Poster Available'
-                                    }`}
-                                className=""
-                            />
-                            <p className="break-words">
-                                {movie.title}
-                            </p>
-                            <p>
-                                {movie?.release_date.slice(0, 4) || '-'}
-                            </p>
-                        </div>
+                        <Link className={`${style.result__card} flex flex-wrap flex-col px-4 py-8`} to={`/movies/${movie.id.toString()}`}>
+                            <div key={movie.id} className="bg-green-200 inline-flex">
+                                <img
+                                    src={`${movie.poster_path ?
+                                        `${MOVIE_POSTER_URL}${movie.poster_path}` :
+                                        'https://placehold.co/200x300?text=No Poster Available'
+                                        }`}
+                                    className={`${style.tile__img}`}
+                                />
+                            </div>
+                            <div className="flex flex-wrap flex-col">
+                                <p className="break-words">
+                                    {movie.title}
+                                </p>
+                                <p>
+                                    {movie?.release_date.slice(0, 4) || '-'}
+                                </p>
+                            </div>
+                        </Link>
                     ))
                 }
 
